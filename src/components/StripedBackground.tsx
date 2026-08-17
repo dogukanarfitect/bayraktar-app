b48a8a892c26959d73440bffa04fd695ae6bc4a8
Dogukan <dogukan.ozturk@arfitect.com>
2026-08-17 11:43:49 +0300

    Update app name and icon in app.json; refactor StripedBackground component for improved stripe rendering

--- src/components/StripedBackground.tsx
+++ src/components/StripedBackground.tsx
@@ -2,24 +2,23 @@ import { StyleSheet, useWindowDimensions, View } from 'react-native';
 
 export function StripedBackground() {
   const { height, width } = useWindowDimensions();
-  const stripeLength = (height + width) * 1.5;
-  const stripeCount = Math.ceil((height + width) / 28) + 4;
+  const canvasSize = Math.ceil(Math.hypot(height, width) * 2);
+  const stripeCount = Math.ceil(canvasSize / 28) + 1;
 
   return (
     <View pointerEvents="none" style={[StyleSheet.absoluteFill, styles.container]}>
-      {Array.from({ length: stripeCount }, (_, index) => (
-        <View
-          key={index}
-          style={[
-            styles.stripe,
-            {
-              height: stripeLength,
-              left: index * 28 - height,
-              top: (height - stripeLength) / 2,
-            },
-          ]}
-        />
-      ))}
+      <View
+        style={{
+          position: 'absolute',
+          width: canvasSize,
+          height: canvasSize,
+          left: (width - canvasSize) / 2,
+          top: (height - canvasSize) / 2,
+          transform: [{ rotate: '45deg' }],
+        }}
+      >
+        {Array.from({ length: stripeCount }, (_, index) => <View key={index} style={[styles.stripe, { height: canvasSize, left: index * 28 }]} />)}
+      </View>
     </View>
   );
 }
@@ -32,6 +31,5 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: StyleSheet.hairlineWidth,
     backgroundColor: 'rgba(255, 255, 255, 0.16)',
-    transform: [{ rotate: '45deg' }],
   },
 });
